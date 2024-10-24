743c52892492247c055e8248cd33847634fd04f2
Ricardo Siena <ricardo.siena@alunoescolassociais.org.br>
2024-10-24 09:53:50 -0300

    Atualiza código JS

--- conversor.js
+++ conversor.js
@@ -1,6 +1,12 @@
 document.getElementById('currency-form').addEventListener('submit', function (event){
 
  // obter valores de entrada do formulário
+ const valor = parseFloat (document.getElementById('valor').value);
+ const daMoeda = document.getElementById('daMoeda').value;
+ const paraMoeda = document.getElementById('paraMoeda').value;
+
+ // definir taxa de câmbio fixa com utilização de objeto
+
  
 
 });
