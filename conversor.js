0758536e6fd1e8659899d8a4b5d30b893dbd7b8e
Ricardo Siena <ricardo.siena@alunoescolassociais.org.br>
2024-10-24 10:09:23 -0300

    Atualiza arquivo JS, melhorando o códigoe adicionando os valores das moedas que são possíveis serem convertidas na aplicação

--- conversor.js
+++ conversor.js
@@ -7,6 +7,9 @@ document.getElementById('currency-form').addEventListener('submit', function (ev
 
  // definir taxa de câmbio fixa com utilização de objeto
 
- 
-
+ const exchangeRates = {
+    USD: {EUR:0.93, BRL:5.71 }, 
+    BRL: {EUR:0.16, USD:0.18 },
+    EUR: {BRL:6.16, USD:1.08 },
+ };
 });
