d379978e05844cc63d058f157e3ee11c0835856f
Ricardo Siena <ricardo.siena@alunoescolassociais.org.br>
2024-10-25 11:38:42 -0300

    atualiza e corrige HTML assim como JS

--- conversor.js
+++ conversor.js
@@ -1,15 +1,26 @@
 document.getElementById('currency-form').addEventListener('submit', function (event){
 
+    event.preventDefault();
+
  // obter valores de entrada do formulário
- const valor = parseFloat (document.getElementById('valor').value);
+ const valor = parseFloat(document.getElementById('Valor').value);
  const daMoeda = document.getElementById('daMoeda').value;
  const paraMoeda = document.getElementById('paraMoeda').value;
 
  // definir taxa de câmbio fixa com utilização de objeto
 
  const exchangeRates = {
-    USD: {EUR:0.93, BRL:5.71 }, 
+    USD: {EUR:0.93, BRL:5.71 },
     BRL: {EUR:0.16, USD:0.18 },
-    EUR: {BRL:6.16, USD:1.08 },
+    EUR: {BRL:6.16, USD:1.08 }
  };
-});
+let valorConvertido
+
+ if (daMoeda === paraMoeda){
+    valorConvertido = valor;
+}else{  valorConvertido = valor * exchangeRates [daMoeda][paraMoeda];       
+    }
+    let conversao = document.getElementById('conversao');
+    conversao.textContent = `resultado ${valorConvertido} ` ;
+
+ });
